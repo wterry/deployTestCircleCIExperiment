f22366ecaca49701b3f90e3e464026d01807f9e7
wterry <wterry@legosoft.com.mx>
2021-03-23 19:47:27 -0600

    prueba de ejecucion en circleci

---
+++ src/main/java/com/legosoft/controller/HelloWorldController.java
@@ -0,0 +1,20 @@
+package com.legosoft.controller;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/helloworld")
+public class HelloWorldController {
+
+
+    @GetMapping("/{nombre}")
+    public ResponseEntity<String> sayHello(@PathVariable("nombre") String nombre) {
+        System.out.println("Peticion reicibida...");
+        return new ResponseEntity("hello " +  nombre + "!", HttpStatus.OK);
+    }
+}
